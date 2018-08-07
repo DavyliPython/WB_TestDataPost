be31fafe63ad81552e84a7ca829cf7bfffa6e24a
asange <luo.c@139.com>
2018-08-07 16:35:52 +0800

    8/7

--- 00_Get_Rate.py
+++ 00_Get_Rate.py
@@ -2,34 +2,47 @@
 
 def dataRate(Fname):
     with open(Fname, 'r') as f:
-        line_limit = 2000
+        line_limit = 20
         i = 0
+        #startingLine = 5   #starting from row
+        #for i in range (startingLine):
+        #     next(f)  # skip the first 20 rows
+
+        line = f.readline()
+        temp_list = line.split()
+
+        if temp_list[0].upper() != "TIME":
+            print("bad data file format")
+            return (-1)
+
+        lastMillisecond = 0
         second_list = []
         for line in f:
-            line_limit = 200
-            temp_list = line.split('\t')
-            temp_num = temp_list[0][0:8]
-            if temp_list[0].isalpha():
-                title = temp_list
-            if temp_list[0][0:2].isdigit():
+
+            temp_list = line.split()
+            iTime = temp_list[0]      # 12:17:44:531
+            iMillisecond = int(iTime.split(":")[3])      # 531
+
+            #if iMillisecond.isdigit():
                 #print(temp_list[0])
                 #print(temp_num)
-                i = i +1
-                second_list.append(temp_num)
-                if i == 1500:break
+            if iMillisecond < lastMillisecond:
+                iMillisecond += 1000  # add 1000ms to the millisecond
+            second_list.append(iMillisecond)
+            if i == line_limit:break
+            # else:
+            #     print("bad data file format")
+            #     return (-1)
+            i += 1
+
+            lastMillisecond = iMillisecond
     #print(second_list)
 
-    # cc_num = second_list[0]
-    # cc_i = 0
-    cc_rate_list = []
-    # for item in second_list:
-    #     if item ==
-    for item in second_list:
-        cc_rate_list.append(second_list.count(item))
-        #print(second_list.count(item))
+    idataRate = 1000/(max(second_list) - min(second_list))* (len(second_list)-1)
 
-    return max(cc_rate_list)
 
-fname = 'C:/00_Work/Work_Python/06_TestPost/test_data2.txt'
+    return int(idataRate)
+
+fname = 'C:/Mydisk/Projects/VPD/100kn/16Hz-100kn-pitch rol and Yawl.txt'
 
 print(dataRate(fname))
